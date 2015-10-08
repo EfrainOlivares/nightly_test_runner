fe3ac5eab0b421847590763b5525d327866a9f4e
EfrainOlivares <efrain@rightscale.com>
2015-10-08 20:18:15 +0000

    Update setup file for new directory structure

--- setup.sh
+++ setup.sh
@@ -1,8 +1,19 @@
-cp todo.sh /usr/local/bin/todo
+#!/bin/bash -ex
+
+# Setup directory for test_runner config files
 mkdir /root/.test_runner
-cp todo.cfg /root/.test_runner/config
-bundle install --system
+
+# add todo shell application and it's config file
+cp todofiles/todo.sh /usr/local/bin/todo
+cp todofiles/todo.cfg /root/.test_runner/config
+
+# jenkins api confif gile
 mkdir /root/.jenkins_api_client
-cp jenkins_login.yml /root/.jenkins_api_client/login.yml
+cp config/jenkins_login.yml /root/.jenkins_api_client/login.yml
+
+# right_api_client config file
 mkdir /root/.right_api_client
-cp api_login.yml /root/.right_api_client/login_test_runner.yml
+cp config/api_login.yml /root/.right_api_client/login_test_runner.yml
+
+# install gems at system level so we can use without bundler, optional
+# bundle install --system
