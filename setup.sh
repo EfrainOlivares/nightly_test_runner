31ea743b0d6a50a68a36bb04d7648de5e872b503
EfrainOlivares <efrain@rightscale.com>
2015-10-28 20:41:18 -0700

    Check if dir exists before creating dot directories

--- setup.sh
+++ setup.sh
@@ -1,14 +1,20 @@
-#!/bin/bash -ex
+#!/bin/bash -e
 
 # Setup directory for test_runner config files
-mkdir /root/.test_runner
+if [[ ! -d /root/.test_runner ]];then
+  mkdir /root/.test_runner
+fi
 cp config/options.yaml /root/.test_runner/
 
 # jenkins api confif gile
-mkdir /root/.jenkins_api_client
+if [[ ! -d /root/.jenkins_api_client ]];then
+  mkdir /root/.jenkins_api_client
+fi
 cp loginyamls/jenkins_login.yml /root/.jenkins_api_client/login.yml
 
 # right_api_client config file
-mkdir /root/.right_api_client
+if [[ ! -d /root/.right_api_client ]];then
+  mkdir /root/.right_api_client
+fi
 cp loginyamls/api_login.yml /root/.right_api_client/login_test_runner.yml
 
